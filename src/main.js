aeceb019712cc1bce7e7840a37c2a8618f809433
Kumar Bishojit Paul <bishojit@gmail.com>
2024-03-14 15:27:09 +0600

    Updated.

--- src/main.js
+++ src/main.js
@@ -1,34 +1,36 @@
-const {
-  app, BrowserWindow, ipcMain, shell,
-  // eslint-disable-next-line import/no-extraneous-dependencies
-} = require('electron');
+/* eslint-disable import/no-extraneous-dependencies */
+const { app, BrowserWindow, ipcMain, shell } = require('electron');
+const winston = require('winston/lib/winston/config');
 const createWindow = require('./utils/createWindow');
 
-app.whenReady().then(() => {
-  // close window
-  ipcMain.on('close-window', () => {
-    app.quit();
-  });
+app
+  .whenReady()
+  .then(() => {
+    // close window
+    ipcMain.on('close-window', () => {
+      app.quit();
+    });
 
-  // Listen for 'minimize-window' event from renderer process
-  ipcMain.on('minimize-window', () => {
-    const focusedWindow = BrowserWindow.getFocusedWindow();
-    if (focusedWindow) {
-      focusedWindow.minimize();
-    }
-  });
+    // Listen for 'minimize-window' event from renderer process
+    ipcMain.on('minimize-window', () => {
+      const focusedWindow = BrowserWindow.getFocusedWindow();
+      if (focusedWindow) {
+        focusedWindow.minimize();
+      }
+    });
 
-  // --Creating Window // it returns win
-  createWindow({ BrowserWindow, shell });
+    // --Creating Window // it returns win
+    createWindow({ BrowserWindow, shell });
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  })
+  .catch((err) => {
+    winston.error(err);
   });
-}).catch((err) => {
-  console.log(err);
-});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
