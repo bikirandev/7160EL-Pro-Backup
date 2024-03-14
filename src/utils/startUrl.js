a307ffa2fca48d635baca96babe7f27875564709
Kumar Bishojit Paul <bishojit@gmail.com>
2024-03-14 14:42:50 +0600

    Updated.

--- src/utils/startUrl.js
+++ src/utils/startUrl.js
@@ -1,13 +1,18 @@
 const isDev = import('electron-is-dev');
+
 const url = import('url');
+
 const path = import('path');
 
-const GetUrl = () => (isDev
-  ? 'http://localhost:3000'
-  : url.format({
-    pathname: path.join(__dirname, '/../../../build/index.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
+const GetUrl = () =>
+  isDev
+    ? 'http://localhost:3000'
+    : url.format({
+        pathname: path.join(__dirname, '/../../../build/index.html'),
+
+        protocol: 'file:',
+
+        slashes: true,
+      });
 
 module.exports = GetUrl;
