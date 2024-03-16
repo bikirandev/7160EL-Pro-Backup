5ccfdb95374c5684055de890c884292772e9a93d
Kumar Bishojit Paul <bishojit@gmail.com>
2024-03-16 14:06:21 +0600

    Updated.

--- src/utils/startUrl.js
+++ src/utils/startUrl.js
@@ -1,13 +1,19 @@
 const url = require('url')
 const path = require('path')
+const fs = require('fs')
 
 const GetUrl = () => {
-  const isDev = process.env.NODE_ENV !== 'production'
+  const isDev = process.env.npm_lifecycle_event === 'electron'
+
+  //--Save a log file to see if the url is correct
+  fs.writeFileSync('log.txt', JSON.stringify(process.env), (err) => {
+    if (err) throw err
+  })
 
   console.log('dev', isDev)
 
   if (isDev) {
-    return 'http://localhost:3000'
+    return    'http://localhost:3000'
   }
 
   console.log('Path', url)
