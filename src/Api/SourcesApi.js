bdce1caec984c344c14e47f434ec6b5313c03d55
Kumar Bishojit Paul <bishojit@gmail.com>
2024-03-29 16:23:35 +0600

    Updated.

--- src/Api/SourcesApi.js
+++ src/Api/SourcesApi.js
@@ -154,79 +154,11 @@ const deleteSource = async (ev, data) => {
   }
 }
 
-// API for start or stop backup process
-const backupAction = async (ev, data) => {
-  try {
-    // Check if database already exists
-    const exData = await getAllDocuments(DB_SOURCE)
-
-    // Check if _id not exists
-    const exId = exData.find((x) => x._id === data._id)
-    if (!exId) {
-      return { error: 1, message: 'Source not exists', data: [] }
-    }
-
-    const nData = {
-      _id: data._id,
-      type: data.type,
-      databaseOrPath: data.databaseOrPath,
-      host: data.host,
-      user: data.user,
-      password: data.password,
-      directory: data.directory,
-      running: data.running,
-      destination: data.destination,
-    }
-
-    const result = await updateDocument(DB_SOURCE, data._id, nData)
-
-    return { error: 0, message: data.running ? 'Backup started' : 'Backup stopped', data: result }
-  } catch (err) {
-    console.log(err)
-    return { error: 1, message: 'Error on start backup', data: [] }
-  }
-}
-
-// API for link destination to source
-const linkDestination = async (ev, data) => {
-  try {
-    // Check if database already exists
-    const exData = await getAllDocuments(DB_SOURCE)
-
-    // Check if _id not exists
-    const exId = exData.find((x) => x._id === data._id)
-    if (!exId) {
-      return { error: 1, message: 'Source not exists', data: [] }
-    }
-
-    const nData = {
-      _id: data._id,
-      type: data.type,
-      databaseOrPath: data.databaseOrPath,
-      host: data.host,
-      user: data.user,
-      password: data.password,
-      directory: data.directory,
-      running: data.running,
-      destination: data.destination,
-    }
-
-    const result = await updateDocument(DB_SOURCE, data._id, nData)
-
-    return { error: 0, message: 'Destination linked', data: result }
-  } catch (err) {
-    console.log(err)
-    return { error: 1, message: 'Error on linking destination', data: [] }
-  }
-}
-
 module.exports = {
   getSources,
   addSource,
   updateSource,
   deleteSource,
-  backupAction,
-  linkDestination,
   backupStart,
   backupStop,
 }
