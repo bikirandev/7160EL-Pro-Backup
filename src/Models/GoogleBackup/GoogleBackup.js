b0a3eb0c85534a8278f94301abe705eb102ed906
Yeasin Arafat <yeasinn2002@gmail.com>
2024-04-06 15:28:51 +0600

    recent backup filtered by sources available on local db

--- src/Models/GoogleBackup/GoogleBackup.js
+++ src/Models/GoogleBackup/GoogleBackup.js
@@ -3,6 +3,7 @@ var path = require('path')
 var fs = require('fs')
 var progress = require('progress-stream')
 const isoToUnix = require('../../utils/isoToUnix')
+const { getAllDocuments, DB_SOURCE } = require('../../utils/PouchDbTools')
 // const { app } = require('electron')
 
 const backupToBucket = async (filePath, destConfig, remoteDir = 'backup', gzip = false) => {
@@ -93,17 +94,37 @@ const getFiles = async (destConfig, remoteDir = '') => {
       prefix: remoteDir,
     })
 
-    const nFiles = files.map((file) => {
-      return {
-        _id: file.id,
-        name: file.name,
-        timeCreated: isoToUnix(file.metadata.timeCreated),
-        timeUpdated: isoToUnix(file.metadata.updated),
-        size: file.metadata.size,
-        sourceId: file.metadata.metadata.sourceId,
-        destinationId: file.metadata.metadata.destinationId,
-      }
-    })
+    // const nFiles = files.map((file) => {
+    //   return {
+    //     _id: file.id,
+    //     name: file.name,
+    //     timeCreated: isoToUnix(file.metadata.timeCreated),
+    //     timeUpdated: isoToUnix(file.metadata.updated),
+    //     size: file.metadata.size,
+    //     sourceId: file.metadata.metadata.sourceId,
+    //     destinationId: file.metadata.metadata.destinationId,
+    //   }
+    // })
+
+    const databaseSources = await getAllDocuments(DB_SOURCE)
+
+    const nFiles = files
+      .map((file) => {
+        return {
+          _id: file.id,
+          name: file.name,
+          timeCreated: isoToUnix(file.metadata.timeCreated),
+          timeUpdated: isoToUnix(file.metadata.updated),
+          size: file.metadata.size,
+          sourceId: file.metadata.metadata.sourceId,
+          destinationId: file.metadata.metadata.destinationId,
+        }
+      })
+      .filter((file) => {
+        // Filter files based on available sources in the database
+        //database sources is an array of all sources in the database
+        return databaseSources.find((source) => source._id === file.sourceId)
+      })
 
     return { error: 0, message: 'List of Backups', data: nFiles }
   } catch (err) {
