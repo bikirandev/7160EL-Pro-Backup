2ff659a55ff601413c3383bd8a3815a319debc6f
Kumar Bishojit Paul <bishojit@gmail.com>
2024-04-17 23:19:28 +0600

    Updated.

--- src/Api/InitApi.js
+++ src/Api/InitApi.js
@@ -40,6 +40,26 @@ const init = async (ev) => {
         message: 'Application Init Success',
         data: {
           appId: appId.data,
+          portsDb: {
+            // Database Ports
+            mysql: 3306,
+            pgsql: 5432,
+            mssql: 1433,
+            oracle: 1521,
+            mongo: 27017,
+            couch: 5984,
+            redis: 6379,
+          },
+          dumpPath: {
+            // Database Dump Path
+            mysql: 'mysqldump',
+            pgsql: 'pg_dump',
+            mssql: 'sqlcmd',
+            oracle: 'exp',
+            mongo: 'mongodump',
+            couch: 'couchdump',
+            redis: 'redis-cli',
+          },
         },
       }
     } catch (err) {
@@ -53,3 +73,26 @@ const init = async (ev) => {
 module.exports = {
   init,
 }
+
+/*
+'cassandra': 9042,
+'neo4j': 7687,
+'elasticsearch': 9200,
+'kafka': 9092,
+'rabbitmq': 5672,
+'activemq': 61616,
+'mqtt': 1883,
+'zookeeper': 2181,
+'hadoop': 9000,
+'spark': 7077,
+'flink': 6123,
+'kubernetes': 6443,
+'docker': 2375,
+'jenkins': 8080,
+'gitlab': 80,
+'sonarqube': 9000,
+'nexus': 8081,
+'artifactory': 8081,
+'harbor': 80,
+'minio': 9000,
+*/
