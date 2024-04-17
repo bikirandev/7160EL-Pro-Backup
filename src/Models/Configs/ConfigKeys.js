65ea171a2beab2f33ef9d2d66b977d8ec963c84b
Yeasin Arafat <yeasinn2002@gmail.com>
2024-04-17 17:22:28 +0600

    configs updated

--- src/Models/Configs/ConfigKeys.js
+++ src/Models/Configs/ConfigKeys.js
@@ -9,7 +9,7 @@ module.exports = {
   CONF_APP_KEY: 'appKey',
   CONF_DEFAULT_DIRECTORY: 'defaultDirectory',
   CONF_SMTP: 'smtp',
-  CONF_DUMP_MSSQL: 'dumpMongo',
+  CONF_DUMP_MSSQL: 'dumpMssql',
   CONF_DUMP_PGSQL: 'dumpPgsql',
   CONF_DUMP_MYSQL: 'dumpMysql',
   configDataPattern,
