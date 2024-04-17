9b9ee91754ed831fe53c0275f7bad02d863dabf4
Kumar Bishojit Paul <bishojit@gmail.com>
2024-04-17 16:58:29 +0600

    Email Sending Updated.

--- src/Api/ConfigSmtpApi.js
+++ src/Api/ConfigSmtpApi.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer')
-const { getSmtpData, setSmtpData } = require('../Models/Configs/ConfigSmtp')
+const { setSmtpData } = require('../Models/Configs/ConfigSmtp')
 
 const setSMTPConfig = async (ev, data) => {
   console.log(ev, data)
@@ -61,26 +61,31 @@ const testSMTPConfig = async (ev, data) => {
     return { error: 1, message: 'Password is required', data: null }
   }
 
+  if (!data.TestEmail) {
+    return { error: 1, message: 'Test Email is required', data: null }
+  }
+
   try {
-    const smtpData = await getSmtpData()
-    if (smtpData.error) {
-      return { error: 1, message: 'Error on getting SMTP Config', data: null }
-    }
+    // const smtpData = await getSmtpData()
+    // if (smtpData.error) {
+    //   return { error: 1, message: 'Error on getting SMTP Config', data: null }
+    // }
 
     // Test SMTP Config
     const transporter = nodemailer.createTransport({
-      host: smtpData.data.hostname,
-      port: smtpData.data.port,
-      secure: true,
+      host: data.hostname,
+      port: data.port,
+      secure: false,
+      requireTLS: true,
       auth: {
-        user: smtpData.data.username,
-        pass: smtpData.data.password,
+        user: data.username,
+        pass: data.password,
       },
     })
 
     // Send Test Email
     const mailOptions = {
-      from: smtpData.data.username,
+      from: `Pro Backup<${data.username}>`,
       to: data.TestEmail,
       subject: 'Test Email',
       text: 'This is a test email from Backup Manager',
